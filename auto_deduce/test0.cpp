8b43a4bda010550d90e112aa599828f00584c54c
vkhristenko <vdkhristenko1991@gmail.com>
2018-03-28 11:48:35 +0200

    need clang of 5.0 at least, installed now compiles

--- auto_deduce/test0.cpp
+++ auto_deduce/test0.cpp
@@ -4,21 +4,23 @@
 #include <type_traits>
 
 template<typename T>
-class sum_t {
+class sum {
 public:
     T value;
 
     template<typename ... Ts>
-    explicit sum_t(Ts&& ... values)
+    explicit sum(Ts&& ... values)
         : value{(values + ...)}
     {}
 };
 
 template <typename ... Ts>
-sum_t(Ts&& ... ts) -> sum_t<std::common_type_t<Ts...>>;
+sum(Ts&& ... ts) -> sum<std::common_type_t<Ts...>>;
 
 int main() {
-    sum_t s {1,2,3,4,5, 5u, 8.0f, 2.3};
+    std::pair p(1,2);
+
+    sum s {1,2,3,4,5, 5u, 8.0f, 2.3};
     std::cout << "value = " << s.value << std::endl;
 
     return 0;
