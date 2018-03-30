2f736b1d28e7d8d7b33063a07a661d80217173f6
vkhristenko <vdkhristenko1991@gmail.com>
2018-03-30 14:06:14 +0200

    finished the multiple consume/produce pattern

--- parallelism/test8.cpp
+++ parallelism/test8.cpp
@@ -35,7 +35,7 @@ void producer(size_t id, size_t items, size_t stock) {
 
         q.push(id * 100 + i);
         par::cout{} << "   Producer " << id << " ---> item " 
-            << setw(3) << q.back << "\n";
+            << setw(3) << q.back() << "\n";
 
         go_consume.notify_all();
         this_thread::sleep_for(90ms);
@@ -48,9 +48,33 @@ void consumer(size_t id) {
     while (!production_stopped || !q.empty()) {
         unique_lock<mutex> lock(q_mutex);
         
-        if (go_consume.wait_for())
+        if (go_consume.wait_for(lock, 1s,
+                                []{ return !q.empty(); })) {
+            par::cout{} << "               item "
+                << setw(3) << q.front() 
+                << " ---> Consumer "
+                << id << "\n";
+            q.pop();
+
+            go_produce.notify_all();
+            this_thread::sleep_for(130ms);
+        }
     }
+
+    par::cout{} << "EXIT: Consumer " << id << "\n";
 }
 
 int main() {
+    vector<thread> producers;
+    vector<thread> consumers;
+
+    for (size_t i=0; i<3; i++)
+        producers.emplace_back(producer, i, 15, 5);
+    for (size_t i=0; i<5; i++)
+        consumers.emplace_back(consumer, i);
+
+    for (auto& t: producers) t.join();
+    production_stopped = true;
+    for (auto& t: consumers) t.join();
+
 }
