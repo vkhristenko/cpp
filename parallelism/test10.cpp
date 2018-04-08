def77965dbb0d1a8ad676fdbc5c1178730029dab
vkhristenko <vdkhristenko1991@gmail.com>
2018-04-08 19:45:28 +0200

    adding an example of a dag

--- parallelism/test10.cpp
+++ parallelism/test10.cpp
@@ -33,6 +33,51 @@ string concat(string const& a, string const& b) {
     return a+b;
 }
 
-int main() {
+string twice(const string& s) {
+    par::cout{} << "3s TWICE " << quoted(s) << "\n";
+    this_thread::sleep_for(3s);
+    return s+s;
+}
 
+template<typename F>
+auto asynchronize(F f) {
+    return [f](auto ... xs) {
+        return [=]() {
+            return async(launch::async, f, xs...);
+        };
+    };
+}
+
+template<typename F>
+auto fut_unwrap(F f) {
+    return [f] (auto ... xs) {
+        return f(xs.get() ...);
+    };
+}
+
+template<typename F>
+auto async_adapter(F f) {
+    return [f] (auto ... xs) {
+        return [=]() {
+            return async(launch::async, fut_unwrap(f), xs()...);
+        };
+    };
+}
+
+int main() {
+    auto pcreate (asynchronize(create));
+    auto pconcat (async_adapter(concat));
+    auto ptwice (async_adapter(twice));
+
+    auto result (
+        pconcat(
+            ptwice(
+                pconcat(
+                    pcreate("foo "),
+                    pcreate("something"))),
+            pconcat(
+                pcreate("this "),
+                pcreate("that "))));
+    std::cout << "setup done. nothing executed yet\n";
+    std::cout << result().get() << "\n";
 }
