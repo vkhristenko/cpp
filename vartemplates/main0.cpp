345a0f08f4ac5ccc9c9bf1308fe9fee53249876b
vkhristenko <vdkhristenko1991@gmail.com>
2018-05-19 12:42:42 +0200

    building my own tuple

--- vartemplates/main0.cpp
+++ vartemplates/main0.cpp
@@ -12,14 +12,44 @@ T adder(T first, Args... args) {
     return first + adder(args...);
 }
 
+//
+// Tuple definition
+//
 template<class ... Ts> struct Tuple {};
-
 template<class T, class ... Ts> 
 struct Tuple<T, Ts...> : Tuple<Ts...> {
     Tuple(T t, Ts ... ts) : Tuple<Ts...>(ts...), tail(t) {}
 
     T tail;
 };
+
+//
+// Tuple element Type holder
+//
+template<size_t, typename > struct elem_type_holder;
+template<typename T, typename ... Ts>
+struct elem_type_holder<0, Tuple<T, Ts...>> {
+    using type = T;
+};
+template<size_t k, typename T, typename ... Ts>
+struct elem_type_holder<k, Tuple<T, Ts...>> {
+    using type = typename elem_type_holder<k-1, Tuple<Ts...>>::type;
+};
+
+//
+// get function on a tuple
+//
+template<size_t k, typename ... Ts>
+typename std::enable_if<k==0, typename elem_type_holder<0, Tuple<Ts...>>::type&>::type
+get(Tuple<Ts...>& t) {
+    return t.tail;
+}
+template<size_t k, typename T, typename ... Ts>
+typename std::enable_if<k!=0, typename elem_type_holder<k, Tuple<T, Ts...>>::type&>::type
+get(Tuple<T, Ts...>& t) {
+    Tuple<Ts...>& base = t;
+    return get<k-1>(base);
+}
 
 int main() {
     std::cout << "hello world" << std::endl;
@@ -34,5 +64,9 @@ int main() {
     
 
     Tuple<double, int, char const*> t1(12.2, 42, "big");
+    std::cout << "f0 = " << get<0>(t1) << std::endl;
+    get<0>(t1) = 15.5;
+    std::cout << "f0 = " << get<0>(t1) << std::endl;
+
     return sizeof(Tuple<double, int, char const*>);
 }
