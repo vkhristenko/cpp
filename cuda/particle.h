403e4314e62c77d349a60ce90c955fb1a5c1ce66
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-20 10:51:01 +0200

    finished example, testing

---
+++ cuda/particle.h
@@ -0,0 +1,11 @@
+#include "v3.h"
+
+class particle {
+private:
+    v3 position, velocity, totalDistance;
+
+public:
+    particle();
+    __host__ __device__ void advance(float);
+    v3 const& getTotalDistance() const;
+};
