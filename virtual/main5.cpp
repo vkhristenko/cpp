f0e2534a1264e38c3a54f0cf5f65995e0ced46ad
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-28 11:43:41 +0200

    checking default ction/dtion for arrays

--- virtual/main5.cpp
+++ virtual/main5.cpp
@@ -3,6 +3,8 @@
 
 class Member {
 public:
+    Member() { std::cout << "default construct member" << std::endl; }
+
     Member(int i)
         : i{i} 
     { std::cout << "construct member " << i << std::endl; }
@@ -24,11 +26,12 @@ class Derived : public Base {
 public:
     // the order below does not matter at all!!!
     Derived() 
-        : m1{1}, m3{3}, m2{2}, Base()
+        : m1{1}, m3{3}, m2{2}, Base(), m4{}
     { std::cout << "constructing Derived" << std::endl; }
     ~Derived() { std::cout << "destructing derived" << std::endl; }
 
     Member m1, m2, m3;
+    Member m4[10];
 };
 
 int main() {
