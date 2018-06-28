92d55209ec6cacce28fea91f01c6f741223ce730
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-28 08:21:07 +0200

    done with virtual destructors and order of construction/destruction

---
+++ virtual/main5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Member {
+public:
+    Member(int i)
+        : i{i} 
+    { std::cout << "construct member " << i << std::endl; }
+
+    ~Member() {
+        std::cout << "destructing member " << i << std::endl;
+    }
+
+    int i;
+};
+
+class Base {
+public:
+    Base() { std::cout << "constructing base" << std::endl; }
+    ~Base() { std::cout << "destructing base" << std::endl; }
+};
+
+class Derived : public Base {
+public:
+    // the order below does not matter at all!!!
+    Derived() 
+        : m1{1}, m3{3}, m2{2}, Base()
+    { std::cout << "constructing Derived" << std::endl; }
+    ~Derived() { std::cout << "destructing derived" << std::endl; }
+
+    Member m1, m2, m3;
+};
+
+int main() {
+    Derived d;
+}
