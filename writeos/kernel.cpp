beeabb8d8a0a99c88162ed8c1dc4652971c8e388
vkhristenko <vdkhristenko1991@gmail.com>
2018-07-29 12:37:47 +0200

    fixing

--- writeos/kernel.cpp
+++ writeos/kernel.cpp
@@ -13,7 +13,7 @@ void printf(char * str)
 typedef void(*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
-extern "C" void call_constructor {
+extern "C" void call_constructors() {
     for (constructor *i = &start_ctors; i != &end_ctors; i++)
         (*i)();
 }
