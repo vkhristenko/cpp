01ce84e5bf656d55403f1d197fd1134a114600ed
vkhristenko <vdkhristenko1991@gmail.com>
2018-07-29 12:34:32 +0200

    add stuff from wyoos

--- writeos/kernel.cpp
+++ writeos/kernel.cpp
@@ -4,15 +4,24 @@
 //
 void printf(char * str) 
 {
-    unsigned short *video_memory = (unsigned short*)0xb8000;
+    static unsigned short *video_memory = (unsigned short*)0xb8000;
 
     for (int i=0; str[i] != '\0'; ++i)
         video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+}
+
+typedef void(*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void call_constructor {
+    for (constructor *i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
 }
 
 extern "C" void kernel_main(void *multiboot_sturcture, unsigned int magic_number) 
 {
     printf("hello world!\n");
 
-    while (true);
+    while (true)
+        ;
 }
