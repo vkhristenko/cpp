b87262f2d5506deaa9deaa806a4518fac32b5d5d
vkhristenko <vdkhristenko1991@gmail.com>
2018-09-22 22:44:59 +0200

    adding mmap example

--- linux/test_io.c
+++ linux/test_io.c
@@ -1,7 +1,9 @@
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void test_write(int fd) {
     char buf[100];
@@ -9,11 +11,21 @@ void test_write(int fd) {
         buf[i] = (char)i;
     int nbytes = write(fd, (char*)buf, 100);
     printf("nbytes = %d written\n", (int)nbytes);
+}
 
+void test_mmap(int fd) {
     lseek(fd, 0, 0);
+
+    void *p = mmap(0, 100, PROT_READ, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED) {
+        printf("mmap failed!\n");
+        exit(1);
+    }
 }
 
 void test_read(int fd) {
+    lseek(fd, 0, 0);
+
     char buf[100];
     size_t nbytes = read(fd, (char*)buf, 100);
     printf("nbytes = %d read\n", (int)nbytes);
@@ -23,12 +35,23 @@ int main() {
     char *path = "test_file.txt";
     int flag = O_RDWR;
     char *mode = "r";
+
+    struct stat st;
     int fd = open(path, flag);
+    fstat (fd, &st);
+    printf("total size in bytes = %d\n", (int)st.st_size);
 
     printf("fd = %d\n", fd);
 
     test_write(fd);
     test_read(fd);
+
+    // determine a page size
+    size_t page_size = (size_t) sysconf (_SC_PAGESIZE);
+    printf("page_size = %dKB\n", (int)page_size/1024);
+    test_mmap(fd);
+
+    close(fd);
 
     return 0;
 }
