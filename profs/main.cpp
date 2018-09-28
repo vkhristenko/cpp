5e34877e55787517cba41127e19410603cbe2262
vkhristenko <vdkhristenko1991@gmail.com>
2018-09-28 16:30:03 +0200

    splitting into functions

--- profs/main.cpp
+++ profs/main.cpp
@@ -4,17 +4,15 @@
 
 #include "defs.hpp"
 
-int main(int argc, char **argv) {
-    std::srand(std::time(nullptr));
+std::vector<Event> generate(unsigned int ne, unsigned int np) {
+    std::vector<Event> tmps;
+    for (auto i=0u; i<ne; i++)
+        tmps.push_back(Event::randomize(np));
+    return tmps;
+}
 
-    unsigned int nevents = std::atoi(argv[1]);
-    unsigned int nparts = std::atoi(argv[2]);
-    std::cout << "runnign with " << nevents << " events and " << nparts << " particles per event" << std::endl;
-    std::vector<Event> events, events_after_filter;
-    for (auto i=0u; i<nevents; i++)
-        events.push_back(Event::randomize(nparts));
-
-    //unsigned int total = 0u;
+std::vector<Event> some_workload(std::vector<Event> events) {
+    std::vector<Event> events_after_filter;
     for (auto const& event: events) {
         auto filter = [](Particle const& p) -> bool {
             return p.E > 1000 and p.x > 10;
@@ -29,6 +27,18 @@ int main(int argc, char **argv) {
 
         events_after_filter.emplace_back(transformation(event), event.id);
     }
+
+    return events_after_filter;
+}
+
+int main(int argc, char **argv) {
+    std::srand(std::time(nullptr));
+
+    unsigned int nevents = std::atoi(argv[1]);
+    unsigned int nparts = std::atoi(argv[2]);
+    std::cout << "runnign with " << nevents << " events and " << nparts << " particles per event" << std::endl;
+    std::vector<Event> events{generate(nevents, nparts)};
+    std::vector<Event> events_after_filter{some_workload(events)};
 
     return 0;
 }
