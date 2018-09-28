823202f629200f2d5cef67592c01a8da54b2e1df
vkhristenko <vdkhristenko1991@gmail.com>
2018-09-28 16:03:25 +0200

    adding cli args

--- profs/main.cpp
+++ profs/main.cpp
@@ -1,12 +1,34 @@
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 #include "defs.hpp"
 
 int main(int argc, char **argv) {
     std::srand(std::time(nullptr));
 
-    auto event = Event::randomize(1000 * 1000);
+    unsigned int nevents = std::atoi(argv[1]);
+    unsigned int nparts = std::atoi(argv[2]);
+    std::cout << "runnign with " << nevents << " events and " << nparts << " particles per event" << std::endl;
+    std::vector<Event> events, events_after_filter;
+    for (auto i=0u; i<nevents; i++)
+        events.push_back(Event::randomize(nparts));
+
+    //unsigned int total = 0u;
+    for (auto const& event: events) {
+        auto filter = [](Particle const& p) -> bool {
+            return p.E > 1000 and p.x > 10;
+        };
+        auto transformation = [&filter](Event const& e) -> std::vector<Particle> {
+            std::vector<Particle> results;
+            for (auto const& p : e.particles)
+                if (filter(p))
+                    results.push_back(p);
+            return results;
+        };
+
+        events_after_filter.emplace_back(transformation(event), event.id);
+    }
 
     return 0;
 }
