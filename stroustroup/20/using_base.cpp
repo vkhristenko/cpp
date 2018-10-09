67f46682740be48bf15618ba79404aecba94037c
vkhristenko <vdkhristenko1991@gmail.com>
2018-10-09 07:25:35 +0200

    ch20: inherit ctors

--- stroustroup/20/using_base.cpp
+++ stroustroup/20/using_base.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct base {
     void f(int) { std::cout << "call base::f" << std::endl; }
@@ -17,7 +18,23 @@ void use(derived d) {
     br.f(1); // call base::f(int)
 }
 
+template<typename T>
+struct Vector : public std::vector<T> {
+public:
+    using size_type = typename std::vector<T>::size_type;
+    using std::vector<T>::vector; // inherit ctors
+
+    T &operator[](size_type i) { check(i); return this->elem(i); }
+    T const &operator[](size_type i) const { check(i); return this->elem(i); }
+
+    void check(size_type i) { 
+        if (this->size() < i) throw std::range_error{"check failed"};
+    }
+};
+
 int main() {
     derived d;
     use(d);
+
+    Vector<int> k {1,2,3,4,5};
 }
