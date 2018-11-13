199d554da26230e49ea362da32fd3fb4cb249dc5
vkhristenko <vdkhristenko1991@gmail.com>
2018-11-13 19:17:01 +0100

    ch28: enable_if done

--- stroustroup/28/concepts_enable_if.cpp
+++ stroustroup/28/concepts_enable_if.cpp
@@ -21,6 +21,31 @@ public:
     void do_something() {}
 };
 
+struct substitution_failure {};
+
+template<typename T>
+struct substitution_succeeded : std::true_type {};
+template<>
+struct substitution_succeeded<substitution_failure> : std::false_type {};
+
+template<typename T>
+struct get_f_result {
+private:
+    template<typename X>
+    static auto check(X const& x) -> decltype(f(x));
+
+    static substitution_failure check();
+
+public:
+    using type = decltype(check(std::declval<T>()));
+};
+
+template<typename T>
+struct has_f 
+    : substitution_succeeded<typename get_f_result<T>::type> {
+
+};
+
 int main() {
     func(1);
     func(X{});
