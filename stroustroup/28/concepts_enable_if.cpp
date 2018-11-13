ffe4014cab7aac56222e4f9ff48932d58ed51780
vkhristenko <vdkhristenko1991@gmail.com>
2018-11-13 13:33:02 +0100

    ch28: select based on constraint

---
+++ stroustroup/28/concepts_enable_if.cpp
@@ -0,0 +1,29 @@
+#include <type_traits>
+#include <iostream>
+
+template<typename T>
+typename std::enable_if<std::is_integral<T>::value, T>::type func(T const& x) {
+    std::cout << "called for integral types" << std::endl;
+    return x;
+}
+
+template<typename T>
+typename std::enable_if<!std::is_integral<T>::value, T>::type func(T const& x) {
+    std::cout << "called for non-integral types" << std::endl;
+    return x;
+}
+
+class X {
+private:
+    int x,y,z;
+
+public:
+    void do_something() {}
+};
+
+int main() {
+    func(1);
+    func(X{});
+
+    return 0;
+}
