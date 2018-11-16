bb7cc7ce8213fab96312894fc2db5b4d51ae7de8
vkhristenko <vdkhristenko1991@gmail.com>
2018-11-16 11:10:41 +0100

    ch28: playing with tuples

--- stroustroup/28/std_tuple.cpp
+++ stroustroup/28/std_tuple.cpp
@@ -1,7 +1,22 @@
+#include <type_traits>
+#include <string>
+
 namespace test {
+
+template<typename T>
+using Add_const_reference = T const&;
+
+template<typename T>
+using Add_reference = T&;
 
 template<typename... Types>
 class tuple;
+
+template<>
+class tuple<> { 
+public:
+    constexpr tuple() {} 
+};
 
 template<typename Head, typename... Tail>
 class tuple<Head, Tail...> : private tuple<Tail...> {
@@ -15,6 +30,45 @@ public:
     template<typename... VValues>
     tuple(tuple<VValues...> const& other)
         : m_head{other.head()}, Inherited{other.tail()} {}
+
+    template<typename... VValues>
+    tuple& operator=(tuple<VValues...> const& other) {
+        m_head = other.head();
+        tail() = other.tail();
+        return *this;
+    }
+
+protected:
+    Head m_head;
+
+private:
+    Add_reference<Head> head() { return m_head; }
+    Add_const_reference<Head> head() const { return m_head; }
+
+    Inherited& tail() { return *this; }
+    Inherited const& tail() const { return *this; }
+};
+
+template<std::size_t N>
+struct print_tuple {
+    template<typename... Ts>
+    typename std::enable_if<(N<sizeof...(Ts))>::type
+    print(std::ostream& os, tuple<Ts...> const& t) const {
+        os << "," << get<N>(t);
+        print_tuple<N+1>{}(os, t);
+    }
+
+    tmeplate<typename... Ts>
+    typename std::enable_if<!(N<sizeof...(Ts))>::type
+    print(std::ostream& os, tuple<Ts...> const&) const {
+
+    }
+};
+
 }
 
+int main() {
+    test::tuple<int, double, std::string> t{1, 5.2, std::string{"5grewgr"}};
+
+    return 0;
 }
