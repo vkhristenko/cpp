446b9e7b4143e6746aa686b68a268179d6e3b03b
vkhristenko <vdkhristenko1991@gmail.com>
2018-11-23 15:01:46 +0100

    ch34: testing allocators

--- stroustroup/34/def_allocator.cpp
+++ stroustroup/34/def_allocator.cpp
@@ -28,7 +28,7 @@ public:
     pointer address(reference x) const noexcept;
     const_pointer address(const_reference x) const noexcept;
 
-    pointer allocate(size_type n, allocator<void>::const_pointer hint = 0);
+    pointer allocate(size_type n, void const* hint = 0);
     void deallocate(pointer p, size_type n);
 
     size_type max_size() const noexcept;
@@ -52,6 +52,45 @@ allocator<T>::allocator(allocator<U> const&) noexcept {}
 
 template<typename T>
 allocator<T>::~allocator() {}
+
+template<typename A>
+struct allocator_traits {
+    using allocator_type = A;
+    using value_type = typename A::value_type;
+    using pointer = typename A::pointer;
+    using const_pointer = Pointer_traits<pointer>::rebind<value_type const>;
+    using void_pointer = Pointer_traits<pointer>::rebind<void>;
+    using const_void_pointer = Pointer_traits<pointer>::rebind<void const>;
+    using difference_type = Pointer_traits<pointer>::difference_type;
+    using size_type = Make_unsigned<difference_type>;
+    using propogate_on_container_copy_assingment = std::false_type;
+    using propogate_on_container_move_assignment = std::false_type;
+    using propogate_on_container_swap = std::false_type;
+
+};
+
+template<typename P>
+struct pointer_traits {
+    using pointer = P;
+    using element_type = T;
+};
+
+template<typename OuterA, typename... InnerA>
+struct scoped_allocator_adaptor : public OuterA { 
+    using Tr = allocator_traits<OuterA>;
+
+    using outer_allocator_type = OuterA;
+//    using inner_allocator_type = 
+    using value_type = typename Tr::value_type;
+    using size_type = typename Tr::size_type;
+    using difference_type = typename Tr::difference_type;
+    using pointer = typename Tr::pointer;
+    using const_pointer = typename Tr:const_pointer;
+    using void_pointer = typename tr::void_pointer;
+    using const_void_pointer = typename Tr::void_pointer;
+    using propogate_on_container_copy_assingment = typename Tr::const_void_pointer;
+    // and more
+};
 
 }
 
