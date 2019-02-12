f1380bdaa8890d24bd748e181d1c9e010b224197
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-12 13:49:52 +0100

    type lists

--- template-metaprog/type_lists.cpp
+++ template-metaprog/type_lists.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-namespace w1 {
+namespace variadic {
 
 template<typename... Ts> struct type_list {};
 
@@ -17,7 +17,7 @@ using prepend_t = typename prepend<T, VariadicList>::type;
 
 }
 
-namespace w2 {
+namespace recursive {
 
 struct null_t {};
 
@@ -30,6 +30,85 @@ struct type_list {
 // prepend an item to a recursive type list
 template<typename RecursiveList, typename T>
 using prepend_t = type_list<T, RecursiveList>;
+
+template<typename List, typename T>
+struct append;
+
+template<typename Head, typename Tail, typename T>
+struct append<type_list<Head, Tail>, T> {
+    using type = type_list<Head, typename append<Tail, T>::type>;
+};
+
+template<typename T>
+struct append<null_t, T> {
+    using type = type_list<T, null_t>;
+};
+
+template<>
+struct append<null_t, null_t> {
+    using type = null_t;
+};
+
+template<typename List, typename T>
+using append_t = typename append<List, T>::type;
+
+
+// test type 
+using test_type = type_list<int, type_list<float, type_list<double, null_t>>>;
+using test_append_type = append_t<test_type, std::pair<int, double>>;
+
+template<typename... Ts>
+struct make;
+
+// case: normal recursion. consume one type per call
+template<typename T, typename... Rest>
+struct make<T, Rest...> {
+    using type = type_list<T, typename make<Rest...>::type>;
+};
+
+// case: Recursion abort, becaues the list of types ran empty
+template<>
+struct make<> { using type = null_t; };
+
+template<typename... Ts>
+using make_t = typename make<Ts...>::type;
+
+template<typename List>
+struct list_content;
+
+template<typename Head, typename Tail>
+struct list_content<type_list<Head, Tail>> {
+    using head = Head;
+    using tail = Tail;
+};
+
+template<>
+struct list_content<null_t> {
+    using head = null_t;
+    using tail = null_t;
+};
+
+template<typename List>
+using head_t = typename list_content<List>::tail;
+
+template<typename List>
+using tail_t = typename list_content<List>::tail;
+
+}
+
+namespace test {
+
+using namespace ::recursive;
+
+using list123 = make_t<int, float, double>;
+
+// little debuggin helper
+template<typename T> class debug_t;
+
+debug_t<list123> d;
+
+using new_list = append_t<list123, char>;
+debug_t<new_list> f;
 
 }
 
