af3bc9c09e9d15d2d7ee75df1567e8e4cc138201
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-16 20:42:00 +0100

    policy design: combining policy classes

--- design_generic_programming/1/policies.hpp
+++ design_generic_programming/1/policies.hpp
@@ -7,6 +7,20 @@ struct OpNewCreator {
     }
 };
 
+namespace test {
+
+template<class T>
+struct OpNewCreator {
+    static T* Create() {
+        return new T;
+    }
+
+protected:
+    ~OpNewCreator() {}
+};
+
+}
+
 template<class T>
 struct MallocCreator {
     static T* Create() {
@@ -42,4 +56,64 @@ class WidgetManager : public CreationPolicy {
 using MyWidgetMgr = WidgetManager<OpNewCreator<Widget>>;
 
 template<template<class> class CreationPolicy>
-class WidgetManager1 : public CreationPolicy<Widget> {};
+class WidgetManager1 : public CreationPolicy<Widget> {
+public:
+    void SwitchPrototype(Widget* pNewPrototype) {
+        CreationPolicy<Widget>& myPolicy = *this;
+        delete myPolicy.GetPrototype();
+        myPolicy.SetPrototype(pNewPrototype);
+    }
+};
+
+namespace test {
+
+template
+<
+    class T,
+    template<class> class CheckingPolicy,
+    template<class> class ThreadingModel
+>
+class SmartPtr;
+
+template<class T> 
+struct NoChecking {
+    static void Check(T*) {}
+};
+
+template<class T>
+struct EnforceNull {
+    class NullPointerException : public std::exception {};
+    static void Check(T* ptr) {
+        if (!ptr) throw NullPointerException{};
+    }
+};
+
+template<class T>
+struct EnsureNotNull {
+    static void Check(T*& ptr) {
+        if (!ptr) ptr = GetDefaultValue();
+    }
+};
+
+template
+<
+    class T,
+    template<class> class CheckingPolicy,
+    template<class> class ThreadingModel
+>
+class SmartPtr 
+    : public CheckingPolicy<T>
+    , public ThreadingModel<T> 
+{
+    T* operator->() {
+        typename ThreadingModel<SmartPtr>::Lock guard{*this};
+        CheckingPolicy<T>::Check(pointee_);
+        return pointee_;
+    }
+
+private:
+    T* pointee_;
+};
+      
+
+}
--- design_generic_programming/1/policies_main.cpp
+++ design_generic_programming/1/policies_main.cpp
@@ -5,6 +5,7 @@ using mgr_t = WidgetManager1<OpNewCreator>;
 void test0() {
     mgr_t mgr{};
     Widget* w = mgr.Create();
+    //mgr.SwitchPrototype(w); will work only for PrototypeCreator policy
 }
 
 int main() {
