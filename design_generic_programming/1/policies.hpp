bb74f0fff2473d20cb4041333cfb34d548a0f5d8
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-16 22:01:08 +0100

    done with policies

--- design_generic_programming/1/policies.hpp
+++ design_generic_programming/1/policies.hpp
@@ -67,11 +67,15 @@ public:
 
 namespace test {
 
+template<class T>
+class DefaultSmartPtrStorage;
+
 template
 <
     class T,
     template<class> class CheckingPolicy,
-    template<class> class ThreadingModel
+    template<class> class ThreadingModel,
+    template<class> class Storage = DefaultSmartPtrStorage
 >
 class SmartPtr;
 
@@ -90,29 +94,58 @@ struct EnforceNull {
 
 template<class T>
 struct EnsureNotNull {
+    static T* GetDefaultValue() { return new T; }
+
     static void Check(T*& ptr) {
         if (!ptr) ptr = GetDefaultValue();
     }
+};
+
+template<class T>
+class DefaultSmartPtrStorage {
+public:
+    using PointerType = T*;
+    using ReferenceType = T&;
+
+protected:
+    PointerType GetPointer() { return pointee_; }
+    void SetPointer(PointerType ptr) { pointee_ = ptr; }
+
+private:
+    PointerType pointee_;
 };
 
 template
 <
     class T,
     template<class> class CheckingPolicy,
-    template<class> class ThreadingModel
+    template<class> class ThreadingModel,
+    template<class> class Storage
 >
 class SmartPtr 
     : public CheckingPolicy<T>
     , public ThreadingModel<T> 
+    , public Storage<T>
 {
     T* operator->() {
         typename ThreadingModel<SmartPtr>::Lock guard{*this};
-        CheckingPolicy<T>::Check(pointee_);
-        return pointee_;
+//        CheckingPolicy<T>::Check(pointee_);
+        CheckingPolicy<T>::Check(this->GetPointer());
+        return this->GetPointer();
     }
 
-private:
-    T* pointee_;
+    template
+    <
+        class T1,
+        template<class> class CP1,
+        template<class> class TM1,
+        template<class> class S1
+    >
+    SmartPtr(const SmartPtr<T1, CP1, TM1, S1>& other)
+        : CheckingPolicy<T>{other}
+        , ThreadingModel<T>{other}
+        , Storage<T>{other}
+    {}
 };
       
 
