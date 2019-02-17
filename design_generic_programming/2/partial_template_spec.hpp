f8eee912d35517e7a28d2be95ffaf4a27c3b2ae6
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-17 09:35:00 +0100

    finished 2.2 partial template specialization for classes and functions

--- design_generic_programming/2/partial_template_spec.hpp
+++ design_generic_programming/2/partial_template_spec.hpp
@@ -8,10 +8,32 @@ class Widget {
 struct ModalDialog;
 struct MyController;
 
+// partial specialization of widget
+template<class Window>
+class Widget<Window, MyController> {
+    // ... partially specialized implementation...
+};
+
 template<>
 class Widget<ModalDialog, MyController> {
     // ... specialized implementation
 };
 
+template<typename T>
+struct Button {};
+
+template<class ButtonArg>
+class Widget<Button<ButtonArg>, MyController> {
+    // ... further specialized implementation...
+};
+
+struct Window{};
+
+template<class T, class U> T Func(U obj); // primary template
+template<class U> void Func<void, U>(U obj); // illegal partial specialization
+// ^^^^^
+// error: function template partial specialization is not
+//      allowed
+template<class T> T Func(Window obj); // legal (overloading)
 
 }
---
+++ design_generic_programming/2/partial_template_spec_main.cpp
@@ -0,0 +1,11 @@
+#include "partial_template_spec.hpp"
+
+using namespace partial;
+
+void test0() {
+    Widget<ModalDialog, MyController> w;
+}
+
+int main() {
+    return 0;
+}
