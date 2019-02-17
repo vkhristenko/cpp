bbeb1aa9ed0ca4c2ff2f6792e0f59f2f2a3dd10c
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-17 15:46:00 +0100

    2.8 TypeInfo wrapper around std::type_info

---
+++ design_generic_programming/2/wrapper_type_info.hpp
@@ -0,0 +1,38 @@
+namespace wrapper {
+
+struct Base {};
+struct Derived : public Base {};
+
+void Fun(Base* pObj) {
+    // compare the two type_info objects corresponding 
+    // to the type of *pObj and Derived
+    if (typeid(*pObj) == typeid(Derived)) {
+        // ... aha, pObj actually points to a Derived object
+    }
+}
+
+class TypeInfo {
+public:
+    // ctors/dtors
+    TypeInfo();
+    TypeInfo(const std::type_info&);
+    TypeInfo(const TypeInfo&);
+    TypeInfo& operator=(const TypeInfo&);
+
+    // compatibility functions
+    bool before(const TypeInfo&) const;
+    const char* name() const;
+
+private:
+    const std::type_info* pInfo_;
+};
+
+// comparison operators
+bool operator==(const TypeInfo&, const TypeInfo&);
+bool operator!=(const TypeInfo&, const TypeInfo&);
+bool operator<(const TypeInfo&, const TypeInfo&);
+bool operator<=(const TypeInfo&, const TypeInfo&);
+bool operator>(const TypeInfo&, const TypeInfo&);
+bool operator>=(const TypeInfo&, const TypeInfo&);
+
+}
