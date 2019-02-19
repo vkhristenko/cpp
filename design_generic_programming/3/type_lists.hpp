2684025436583972c9802a640f40253ee70c24d1
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-19 10:13:14 +0100

    3: basic operations on type lists

--- design_generic_programming/3/type_lists.hpp
+++ design_generic_programming/3/type_lists.hpp
@@ -16,30 +16,72 @@ namespace TL {
 
 // typelist algorithms
 template<typename... Ts>
-struct make;
+struct Make;
 
 template<typename T, typename... Ts>
-struct make<T, Ts...> {
-    using type = TypeList<T, typename make<Ts...>::type>;
+struct Make<T, Ts...> {
+    using type = TypeList<T, typename Make<Ts...>::type>;
 };
 
 template<typename T>
-struct make<T> {
+struct Make<T> {
     using type = TypeList<T, NullType>;
 };
 
 template<>
-struct make<> {
+struct Make<> {
     using type = TypeList<NullType, NullType>;
 };
 
-/*
-#define TYPELIST_1(T1) TypeList<T1, NullType>
-#define TYPELIST_2(T1, T2) TypeList<T1, TYPELIST_1(T2)>
-#define TYPELIST_3(T1, T2, T3) TypeList<T1, TYPELIST_2(T2, T3)>
-#define TYPELIST_4(T1, T2, T3, T4) TypeList<T1, TYPELIST_3(T2, T3, T4)>
-#define TYPELIST_5(T1, T2, T3, T4, T5) TypeList<T1, TYPELIST_4(T2, T3, T4, T5)>
-*/
+template <class TList> 
+struct Length;
+
+template<>
+struct Length<NullType> {
+    enum { value = 0 };
+};
+
+template<class T, class U>
+struct Length<TypeList<T, U>> {
+    enum { value = 1 + Length<U>::value };
+};
+
+template<class TList, unsigned int index>
+struct TypeAt;
+
+template<class Head, class Tail>
+struct TypeAt<TypeList<Head, Tail>, 0> {
+    typedef Head Result;
+};
+
+template<class Head, class Tail, unsigned int index>
+struct TypeAt<TypeList<Head, Tail>, index> {
+    typedef typename TypeAt<Tail, index-1>::Result Result;
+};
+
+template<class TList, class T> 
+struct IndexOf;
+
+template<class T>
+struct IndexOf<NullType, T> {
+    enum { value = -1 };
+};
+
+template<class T, class Tail>
+struct IndexOf<TypeList<T, Tail>, T> {
+    enum { value = 0 };
+};
+
+template<class Head, class Tail, class T>
+struct IndexOf<TypeList<Head, Tail>, T> {
+private:
+    enum { temp = IndexOf<Tail, T>::value };
+
+public:
+    enum { value = temp == -1 ? -1 : 1 + temp };
+}
+
+}
 
 #define TYPELIST_1(T1) TL::make<T1>::type
 #define TYPELIST_2(T1, T2) TL::make<T1, T2>::type
@@ -47,6 +89,4 @@ struct make<> {
 #define TYPELIST_4(T1, T2, T3, T4) TL::make<T1, T2, T3, T4>::type
 #define TYPELIST_5(T1, T2, T3, T4, T5) TL::make<T1, T2, T3, T4, T5>::type
 
-}
-
-#endif // type_lists_hpp
+#endif // type_lists_hp
--- design_generic_programming/3/type_lists_main.cpp
+++ design_generic_programming/3/type_lists_main.cpp
@@ -1,11 +1,18 @@
+#include <typeinfo>
+
 #include "../2/types.hpp"
 #include "type_lists.hpp"
+    
+typedef TypeList<int, NullType> OneTypeOnly;
+typedef TypeList<char, TypeList<signed char, TypeList<unsigned char, NullType>>>
+    AllCharTypes;
+using SignedIntegrals = TL::Make<signed char, short int, int, long int>::type;
 
 void test0() {
-    typedef TypeList<int, NullType> OneTypeOnly;
-    typedef TypeList<char, TypeList<signed char, TypeList<unsigned char, NullType>>>
-        AllCharTypes;
-    using SignedIntegrals = TL::make<signed char, short int, int, long int>::type;
+}
+
+void test1() {
+    std::type_info* intsRTTI[TL::Length<SignedIntegrals>::value];
 }
 
 int main() {
