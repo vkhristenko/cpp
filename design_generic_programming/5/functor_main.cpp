156164faf69dedf721087b07357b297d0268e166
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-24 19:39:17 +0100

    5.7: can use lambdas, pointer to functions, functor objects

--- design_generic_programming/5/functor_main.cpp
+++ design_generic_programming/5/functor_main.cpp
@@ -4,13 +4,37 @@
 #include "../3/type_lists.hpp"
 #include "functor.hpp"
 
+int test_functor(int, double) { return 1.0; }
+
+void test_functor(int);
+
 void test0() {
     Functor<int, TL::Make<int, double>::type> func{[](int a, double b) -> int { return static_cast<int>(b) + a; }};
     auto result = func(3, 8.0);
     std::cout << "result = " << result << std::endl;
+    
+    typedef int (*TF)(int, double);
+    TF pF = test_functor;
+    Functor<int, TL::Make<int, double>::type> func1{pF};
+    Functor<int, TL::Make<int, double>::type> func2{static_cast<TF>(test_functor)};
+    std::cout << "result = " << func1(5, 10) << std::endl;
+    std::cout << "result = " << func2(10, 10) << std::endl;
+}
+
+int test_function(int a, int b) {
+    return a+b;
+}
+
+void test1() {
+    typedef int (*func_type)(int, int);
+    func_type s = &test_function;
+    func_type s1 = test_function;
+    std::cout << "result = " << (*s)(1,1) << std::endl;
+    std::cout << "result = " << s1(1,1) << std::endl;
 }
 
 int main() {
     test0();
+    test1();
     return 0;
 }
