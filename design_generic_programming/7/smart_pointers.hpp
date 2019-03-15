e35086b289232249a7f853d9747d760670d7acc8
vkhristenko <vdkhristenko1991@gmail.com>
2019-03-15 09:01:57 +0100

    ch7: playing with smart pointers

--- design_generic_programming/7/smart_pointers.hpp
+++ design_generic_programming/7/smart_pointers.hpp
@@ -6,13 +6,18 @@ namespace simple {
 template<class T>
 class SmartPtr {
 public:
-    explicit SmartPtr(T* pointee) : pointee_{pointee} {}
+    explicit SmartPtr(T* pointee = nullptr) : pointee_{pointee} {}
     SmartPtr(SmartPtr&& other) {
         pointee_ = other.pointee_;
         other.pointee_ = nullptr;
     }
     SmartPtr& operator=(SmartPtr&& other) {
+        if (this == other) 
+            return *this;
+
+        delete pointee_;
         pointee_ = other.pointee_;
+        other.pointee_ = nullptr;
         return *this;
     }
 
@@ -29,15 +34,51 @@ public:
         return pointee_;
     }
 
+    //
+    // conversion operators
+    //
+    operator T*() {
+        return pointee_;
+    }
+
+    /*
+    operator void*() {
+        return pointee_;
+    }*/
+
+    // 
+    // various operator overloading
+    //
+    bool operator!() const {
+        // enables "if (!sp) ..."
+        return pointee_ == 0;
+    }
+
+    inline friend bool operator==(const SmartPtr& lhs, const T* rhs) {
+        return lhs.pointee_ == rhs;
+    }
+
+    inline friend bool operator==(const T* lhs, const SmartPtr& rhs) {
+        return lhs == rhs.pointee_;
+    }
+
+    inline friend bool operator!=(const SmartPtr& lhs, const T* rhs) {
+        return lhs.pointee_ != rhs;
+    }
+
+    inline friend bool operator!=(const T* lhs, const SmartPtr& rhs) {
+        return lhs != rhs.pointee_;
+    }
+
 private:
     T* pointee_;
 };
-
-}
 
 template<class T> T* GetImpl(SmartPtr<T>& sp);
 template<class T> T*& GetImplRef(SmartPtr<T>& sp);
 template<class T> void Reset(SmartPtr<T>& sp, T* source);
 template<class T> void Release(SmartPtr<T>& sp, T*& destination);
+
+}
 
 #endif
--- design_generic_programming/7/smart_pointers_test.cpp
+++ design_generic_programming/7/smart_pointers_test.cpp
@@ -13,6 +13,21 @@ void test0() {
     (*pw).func();
 }
 
+struct Apple {};
+struct Orange {};
+
+void test1() {
+    using simple::SmartPtr;
+    SmartPtr<Apple> sp1;
+    SmartPtr<Apple> sp2;
+    SmartPtr<Orange> sp3;
+
+    if (sp1 == sp2)
+        std::cout << "s1 equal s2\n";
+    else 
+        std::cout << "s2 is not equal s2\n";
+}
+
 int main() {
     test0();
     return 0;
