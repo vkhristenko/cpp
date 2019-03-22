d1f1a044dd3a6f5502fcc9a90a0819d9b9604553
vkhristenko <vdkhristenko1991@gmail.com>
2019-03-22 22:31:08 +0100

    comparison operators

--- design_generic_programming/7/smart_pointers.hpp
+++ design_generic_programming/7/smart_pointers.hpp
@@ -1,17 +1,24 @@
 #ifndef smart_pointers_hpp
 #define smart_pointers_hpp
 
+#define PRETTIFY \
+    std::cout << __PRETTY_FUNCTION__ << std::endl
+
 namespace simple {
 
 template<class T>
 class SmartPtr {
 public:
-    explicit SmartPtr(T* pointee = nullptr) : pointee_{pointee} {}
+    explicit SmartPtr(T* pointee = nullptr) : pointee_{pointee} {
+        PRETTIFY;
+    }
     SmartPtr(SmartPtr&& other) {
+        PRETTIFY;
         pointee_ = other.pointee_;
         other.pointee_ = nullptr;
     }
     SmartPtr& operator=(SmartPtr&& other) {
+        PRETTIFY;
         if (this == other) 
             return *this;
 
@@ -24,22 +31,71 @@ public:
     SmartPtr(const SmartPtr&) = delete;
     SmartPtr& operator=(const SmartPtr&) = delete;
 
-    ~SmartPtr() { delete pointee_; }
+    ~SmartPtr() { 
+        PRETTIFY;
+        delete pointee_; 
+    }
 
     T& operator*() const {
+        PRETTIFY;
         return *pointee_;
     }
 
     T* operator->() const {
+        PRETTIFY;
         return pointee_;
     }
 
     //
     // conversion operators
     //
+    /*
     operator T*() {
+        PRETTIFY;
         return pointee_;
     }
+
+    operator void*() {
+        PRETTIFY;
+        return pointee_; // error: ambiguous conversion of delete expression of type
+    }*/
+
+    //
+    // comparison operators
+    //
+    bool operator!() const {
+        return pointee_ == 0;
+    }
+
+    bool operator==(const SmartPtr& rhs) const { 
+        return *this == rhs.pointee_;
+    }
+
+    template<class U>
+    bool operator==(const SmartPtr<U>& rhs) const {
+        return pointee_ == rhs.pointee_;
+    }
+
+    inline friend bool operator==(const SmartPtr& lhs, const T* rhs) {
+        return lhs.pointee_ == rhs;
+    }
+
+    inline friend bool operator==(const T* lhs, const SmartPtr& rhs) {
+        return lhs == rhs.pointee_;
+    }
+
+    inline friend bool operator!=(const SmartPtr& lhs, const T* rhs) {
+        return lhs.pointee_ != rhs;
+    }
+
+    inline friend bool operator!=(const T* lhs, const SmartPtr& rhs) {
+        return lhs != rhs.pointee_;
+    }
+
+    /*
+    operator bool() {
+        return pointee_ != 0;
+    }*/
 
     /*
     operator void*() {
@@ -49,6 +105,7 @@ public:
     // 
     // various operator overloading
     //
+    /*
     bool operator!() const {
         // enables "if (!sp) ..."
         return pointee_ == 0;
@@ -69,12 +126,39 @@ public:
     inline friend bool operator!=(const T* lhs, const SmartPtr& rhs) {
         return lhs != rhs.pointee_;
     }
+    */
 
 private:
     T* pointee_;
+
+    class Tester {
+        void operator delete(void*);
+    };
+
+public:
+    operator Tester*() const {
+        if (!pointee_) return 0;
+        static Tester test;
+        return &test;
+    }
+
+private:
+    explicit operator T*() {
+        PRETTIFY;
+        return pointee_;
+    }
+
+    T* Get() { return pointee_; }
+
+    template<class U> 
+    friend
+    U* GetImpl(SmartPtr<U>&); 
 };
 
-template<class T> T* GetImpl(SmartPtr<T>& sp);
+template<class T> T* GetImpl(SmartPtr<T>& sp) {
+    return sp.Get();
+//    return sp.operator T*();
+}
 template<class T> T*& GetImplRef(SmartPtr<T>& sp);
 template<class T> void Reset(SmartPtr<T>& sp, T* source);
 template<class T> void Release(SmartPtr<T>& sp, T*& destination);
--- design_generic_programming/7/smart_pointers_test.cpp
+++ design_generic_programming/7/smart_pointers_test.cpp
@@ -6,13 +6,17 @@ struct Widget {
     void func() { std::cout << "hello from Widget::func\n"; }
 };
 
+using namespace simple;
+
 void test0() {
-    using simple::SmartPtr;
     SmartPtr<Widget> pw{new Widget};
     pw->func();
     (*pw).func();
 }
 
+
+
+/*
 struct Apple {};
 struct Orange {};
 
@@ -27,8 +31,77 @@ void test1() {
     else 
         std::cout << "s2 is not equal s2\n";
 }
+*/
+
+struct TestStruct {};
+
+void Fun(TestStruct* p) {
+    std::cout << "inside Fun\n";
+}
+
+void test2() {
+    SmartPtr<TestStruct> sp{new TestStruct};
+    SmartPtr<TestStruct> sp1{new TestStruct};
+    TestStruct* ptr;
+    std::cout << "before Fun\n";
+    Fun(GetImpl(sp));
+    std::cout << "after Fun\n";
+
+    /*
+    if (sp)
+        std::cout << "if (sp)\n";
+        */
+
+    if (!sp)
+        std::cout << "if (!sp)\n";
+    
+    if (sp == 0)
+        std::cout << "if (sp == 0)\n";
+
+    if (sp == sp1)
+        std::cout << "if (sp == sp1)\n";
+    
+    if (sp == ptr)
+        std::cout << "if (sp == ptr)\n";
+}
+
+struct Apple {};
+struct Orange {};
+
+void test3() {
+    PRETTIFY;
+
+    SmartPtr<Apple> sp1{new Apple};
+    SmartPtr<Apple> sp2{new Apple};
+   
+    if (sp1 == sp2)
+        std::cout << "if (sp1 == sp2)\n";
+
+    /*
+    if (sp1 * 5 == 200)
+        std::cout << "if (sp1 * 5 == 200)\n";
+        */
+}
+
+struct Base {};
+struct Derived : Base {};
+
+void test4() {
+    PRETTIFY;
+
+    SmartPtr<Base> p1;
+    Derived* ptr;
+
+    if (p1 == ptr)
+        std::cout << "if (p1 == ptr)\n";
+
+    if (p1)
+        std::cout << "if (p1)\n";
+}
 
 int main() {
     test0();
+    test2();
+    test3();
     return 0;
 }
