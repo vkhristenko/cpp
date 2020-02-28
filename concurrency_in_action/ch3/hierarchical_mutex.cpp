b40961e08863c90eae43c1796357ee8916320318
vkhristenko <vdkhristenko1991@gmail.com>
2020-02-28 17:13:57 +0100

    [ccia] mutexes, hierarchical mutexes

---
+++ concurrency_in_action/ch3/hierarchical_mutex.cpp
@@ -0,0 +1,120 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <queue>
+
+void test0();
+
+int main() {
+    test0();
+}
+
+std::queue<std::exception_ptr> exc_queue;
+
+class hierarchical_mutex {
+    std::mutex internal_mutex;
+    unsigned long const hierarchy_value;
+    unsigned long previous_hierarchy_value;
+    static thread_local unsigned long this_thread_hierarchy_value;
+
+    void check_for_hierarchy_violation() {
+        if (this_thread_hierarchy_value <= hierarchy_value)
+            throw std::logic_error{"mutex hierarchy violated"};
+    }
+
+    void update_hierarchy_value() {
+        previous_hierarchy_value = this_thread_hierarchy_value;
+        this_thread_hierarchy_value = hierarchy_value;
+    }
+
+public:
+    explicit hierarchical_mutex(unsigned long value)
+        : hierarchy_value{value}
+        , previous_hierarchy_value{0}
+    {}
+
+    void lock() {
+        check_for_hierarchy_violation();
+        internal_mutex.lock();
+        update_hierarchy_value();
+    }
+
+    void unlock() {
+        this_thread_hierarchy_value = previous_hierarchy_value;
+        internal_mutex.unlock();
+    }
+
+    bool try_lock() {
+        check_for_hierarchy_violation();
+        if (!internal_mutex.try_lock())
+            return false;
+        update_hierarchy_value();
+        return true;
+    }
+};
+
+thread_local unsigned long hierarchical_mutex::this_thread_hierarchy_value{ULONG_MAX};
+
+hierarchical_mutex high_level_mutex{10000};
+hierarchical_mutex low_level_mutex{5000};
+
+int do_low_level_stuff() { return 0; }
+
+int low_level_func() {
+    std::lock_guard<hierarchical_mutex> lk{low_level_mutex};
+    return do_low_level_stuff();
+}
+
+void high_level_stuff(int some_param) {}
+
+void high_level_func() {
+    std::lock_guard<hierarchical_mutex> lk{high_level_mutex};
+    high_level_stuff(low_level_func());
+}
+
+void thread_a() {
+    high_level_func();
+}
+
+hierarchical_mutex other_mutex{100};
+void do_other_stuff() {}
+
+void other_stuff() {
+    high_level_func();
+    do_other_stuff();
+}
+
+void thread_b() {
+    try {
+        std::lock_guard<hierarchical_mutex> lk{other_mutex};
+        other_stuff();
+    } catch (std::logic_error const& exc) {
+        exc_queue.push(std::current_exception());
+    }
+}
+
+class thread_guard {
+    std::thread t;
+public:
+    explicit thread_guard(std::thread t_) : t{std::move(t_)} {}
+    ~thread_guard() { if (t.joinable()) t.join(); }
+};
+
+void test0_wrapper() {
+    thread_guard a{std::thread{thread_a}};
+    thread_guard b{std::thread{thread_b}};
+}
+
+void test0() {
+    test0_wrapper();
+    while (!exc_queue.empty()) {
+        try {
+            auto exc = exc_queue.front();
+            if (exc)
+                std::rethrow_exception(exc);
+        } catch (std::logic_error const& exc) {
+            std::cout << "standard exception caught " << exc.what() << std::endl;
+        }
+        exc_queue.pop();
+    }
+}
--- concurrency_in_action/ch3/mutexes.cpp
+++ concurrency_in_action/ch3/mutexes.cpp
@@ -9,9 +9,11 @@
 #include <stack>
 
 void test0();
+void test1();
 
 int main() {
     test0();
+    test1();
 }
 
 std::list<int> my_list;
@@ -46,7 +48,9 @@ void test0() {
 }
 
 struct empty_stack : std::exception {
-    const char* what() const throw();
+    const char* what() const throw() {
+        return "empty stack exception";
+    }
 };
 
 template<typename T>
@@ -62,13 +66,15 @@ public:
     void push(T new_value) {
         std::lock_guard<std::mutex> lock{m};
         data.push(new_value);
+        std::cout << "pushing value " << new_value << std::endl;
     }
     std::shared_ptr<T> pop() {
         std::lock_guard<std::mutex> lock{m};
         if (data.empty()) throw empty_stack{};
 
-        std::shared_ptr<T> const res = std::make_shared(data.top());
+        std::shared_ptr<T> const res = std::make_shared<T>(data.top());
         data.pop();
+        std::cout << "popping value " << *res << std::endl;
         return res;
     }
     void pop(T& value) {
@@ -86,3 +92,34 @@ private:
     std::stack<T> data;
     mutable std::mutex m;
 };
+
+void push_func(threadsafe_stack<int> &my_stack, int value) {
+    my_stack.push(value);
+}
+
+void pop_func(threadsafe_stack<int> &my_stack) {
+    try {
+        auto value = my_stack.pop();
+    } catch (empty_stack const& exc) {
+        std::cout << exc.what() << '\n';
+    }
+}
+
+void test1() {
+    std::cout << std::endl << "<<<<<<<<<<<<<\n"
+        << "test 1\n"
+        << ">>>>>>>>>>>>\n";
+    threadsafe_stack<int> my_stack;
+    
+    std::vector<std::thread> pushers;
+    std::vector<std::thread> poppers;
+    for (int i=0; i<50; i++)
+        pushers.push_back(std::thread{push_func, std::ref(my_stack), i});
+    for (int i=0; i<100; i++)
+        poppers.push_back(std::thread{pop_func, std::ref(my_stack)});
+    for (int i=0; i<50; i++)
+        pushers.push_back(std::thread{push_func, std::ref(my_stack), i+50});
+
+    for (auto &t : pushers) t.join();
+    for (auto &t : poppers) t.join();
+}
